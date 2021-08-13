f94f76dc9d5f50d9f2af95eb654d05fc97a8dfd5
Sammie <Lucidareverie@outlook.com>
2021-08-14 00:42:13 +0100

    cleaned up anim file - made it dry

--- assets/js/anim.js
+++ assets/js/anim.js
@@ -4,11 +4,6 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.fromTo("#down-arrow", {opacity: 0}, {delay: 3, duration: 2, opacity: 1});
 gsap.to("#down-arrow",{y: "40%", duration: 1, yoyo: true, ease: "sine.in", repeat: -1, delay: 4});
 
-
-/*syntax for timeline animations
-const timeline = gsap.timeline(defaults:{ });
-timeline.from("[QUERYSELECTOR]", {PROPERTIES}, DELAY)
-        .from()*/
 //Animation for welcome words
 const welcomeWords = ["Howdy!", "Bonjour!", "¡Hola!", "Ciao!", "E kãro" ,"你好", "こんにちは", "ٱلسَّلَامُ عَلَيْكُمْ‎"]
 gsap.to(".cursor", {opacity: 0, duration: 0.5, yoyo: true, ease: "power2.inOut", repeat: -1});
@@ -23,45 +18,25 @@ welcomeWords.forEach(word =>{
 
 gsap.from("#welcome-content>h3",{delay: 2, duration: 1, opacity: 0, ease: "sine.in"});
 
-function welcomeDisplay(){
-    gsap.to(".text", {duration: 1, text: welcomeWords[0]});
-}
-
-
-
-
-
 // animation for skill icons - to start on scrol to the section
-
-
-let skillsTimeLine = gsap.timeline({defaults: {duration: 1.5}, scrollTrigger: {
-    trigger: ".skill-item",
-    toggleActions: "resume pause resume pause",
-    start: "top center",
-}}); 
+let skillsTimeLine = gsap.timeline({
+    defaults: {duration: 1.5}, 
+    scrollTrigger: {
+        trigger: ".skill-item",
+        toggleActions: "resume pause resume pause",
+        start: "top center",
+    }
+}); 
 skillsTimeLine.from(".skill-item", {opacity: 0, stagger: 0.5, delay: 0.5, ease: "circ"})
               .to(".static", 0.05, {onRepeat: staticAnim , repeat: -1},"<")
               .to(".static", {opacity: 0, stagger: 0.5,}, "<2.5");
 
-// function skillsFadeIn(){
-
-    // gsap.from(".skill-item", {
-    //     scrollTrigger: {
-    //         trigger: ".skill-item",
-    //         toggleActions: "resume pause resume pause",
-    //         markers: true,
-    //         start: "top center",
-    //     }, 
-    //     opacity: 0, 
-    //     stagger: 0.5,
-    //     delay: 0.5, 
-    //     duration: 2,
-    //     ease: "circ"
-    // })
-
-// }
-
+// helper functions
 
 function staticAnim(){
-    gsap.set(".static", {backgroundPosition: Math.floor(Math.random() * 100) + 1 + "% " + Math.floor(Math.random() * 10) + 1 + "%"})
+    gsap.set(".static", {backgroundPosition: Math.floor(Math.random() * 100) + 1 + "% " + Math.floor(Math.random() * 10) + 1 + "%"});
+}
+
+function welcomeDisplay(){
+    gsap.to(".text", {duration: 1, text: welcomeWords[0]});
 }
